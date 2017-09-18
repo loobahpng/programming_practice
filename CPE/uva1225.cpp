c892cde7084f9a27be27dd1091d9de7ea808d8dd
loobahpng <m300574@hasel5.mpimet.mpg.de>
2017-09-18 14:33:02 +0200

    add comment

--- CPE/uva1225.cpp
+++ CPE/uva1225.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int ndata;
-    int N;
+    int ndata; //number of datasets
+    int N; //write from 1,2,3...to N
     cin>>ndata;
     for (int i=1;i<=ndata;i++){
-        int numbers[10]={0,0,0,0,0,0,0,0,0,0};
+        int numbers[10]={0,0,0,0,0,0,0,0,0,0}; //times of appearences of 0,1,2...8,9
         cin >>N;
         for (int j=1;j<=N;j++){
             for (int k=0;k<=9;k++){
-                if (j>=1000 && k==j/1000%10) numbers[k]++;
-                if (j>=100 && k==j/100%10) numbers[k]++;
-                if (j>=10 && k==j/10%10) numbers[k]++;
-                if (k==j%10) numbers[k]++;
+                if (j>=1000 && k==j/1000%10) numbers[k]++; //千位
+                if (j>=100 && k==j/100%10) numbers[k]++; //百位
+                if (j>=10 && k==j/10%10) numbers[k]++; //十位
+                if (k==j%10) numbers[k]++; //個位
             }
         }
         for (int j=0;j<=9;j++){
