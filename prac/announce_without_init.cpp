7fd8772b3df92b5073769be14b13fa7a6dbb552c
loobahpng <uiqlzjpioztm@gmail.com>
2017-09-19 11:12:30 +0200

    rm prac/ dir

--- prac/announce_without_init.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int i;
-    int *ptr;
-    cout<<"i= "<<i<<endl;
-    cout<<"*ptr= "<<*ptr<<endl;
-}
--- prac/single_float.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    float x=10000001;
-    cout<<"x= "<<x<<endl;
-}
